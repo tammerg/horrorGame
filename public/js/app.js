1db5401a7777a6714a04c1807d65bd5fb46e4d29
CB <cbmoate@gmail.com>
2016-08-19 15:46:21 -0400

    views are firing off now.  we even have nested options!  Huzzah

--- public/js/app.js
+++ public/js/app.js
@@ -1,33 +1,23 @@
-angular.module('views', ['ui-router',])
-.config(['$stateProvider', '$urlRouterProvider','$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider){
-  $urlRouterProvider.otherwise('/');
-  //differnt page states
-  $stateProvider
-    .state('main', {
-      url: '/',
-      views: {
-        '':{
-          templateUrl: 'views/main.html'
+angular.module('views', [
+  'ui.router', 
+  ])
+  .config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider) {
+    $urlRouterProvider.otherwise('/');
+    $stateProvider
+      .state('main', {
+        url: '/',
+        views: {
+          '': {
+            templateUrl: '/views/main.html'
           },
-        'body@main': {
-          templateUrl: 'views/main-body.html'
+          'body@main': {
+            templateUrl: '/views/main-body.html'
           }
-      }
-    })
-    .state('death', {
-      url: '/death',
-      views: {
-        '':{
-          templateUrl: '/views/death.html'
-          },
-        'body@death': {
-          templateUrl: '/views/death-body.html'
-          }
-      }
-    })
+        }
+      })
 
-  $locationProvider.html5Mode({
-    enabled: true,
-    requireBase: false
-  });
-}]);
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false
+    });
+  }]);
